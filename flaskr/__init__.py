ec063d6d36b5517a98434f6b304b659c80e00afb
Unknown <julien.piot@ipilyon.net>
2019-08-30 16:42:12 +0200

    comment ok and finish

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,13 +7,12 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY="dev", DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -25,13 +24,16 @@ def create_app(test_config=None):
         pass
 
     from . import db
+
     db.init_app(app)
 
     from . import auth
+
     app.register_blueprint(auth.bp)
-    
+
     from . import groupe
+
     app.register_blueprint(groupe.bp)
-    app.add_url_rule('/', endpoint='index')
+    app.add_url_rule("/", endpoint="index")
 
     return app
--- flaskr/groupe.py
+++ flaskr/groupe.py
@@ -1,100 +1,106 @@
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
-)
+from flask import Blueprint, flash, g, redirect, render_template, request, url_for
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
-bp = Blueprint('groupe', __name__)
+bp = Blueprint("groupe", __name__)
 
-@bp.route('/')
+
+@bp.route("/")
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, titre, description, commentaire, groupe_id, username'
-        ' FROM groupe p JOIN user u ON p.groupe_id = u.id'
-        ' ORDER BY groupe_id DESC'
+        "SELECT p.id, titre, description, groupe_id, username"
+        " FROM groupe p JOIN user u ON p.groupe_id = u.id"
+        " ORDER BY groupe_id DESC"
     ).fetchall()
-    return render_template('groupe/index.html', posts=posts)
+    comms = db.execute("SELECT commentaire, groupe_id" " FROM commentaire").fetchall()
+    return render_template("groupe/index.html", posts=posts, comms=comms)
 
-@bp.route('/create', methods=('GET', 'POST'))
+
+@bp.route("/create", methods=("GET", "POST"))
 @login_required
 def create():
-    if request.method == 'POST':
-        titre = request.form['titre']
-        description = request.form['description']
+    if request.method == "POST":
+        titre = request.form["titre"]
+        description = request.form["description"]
         error = None
 
         if not titre:
-            error = 'Mettre un titre.'
+            error = "Mettre un titre."
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO groupe (titre, description, groupe_id)'
-                ' VALUES (?, ?, ?)',
-                (titre, description, g.user['id'])
+                "INSERT INTO groupe (titre, description, groupe_id)"
+                " VALUES (?, ?, ?)",
+                (titre, description, g.user["id"]),
             )
             db.commit()
-            return redirect(url_for('groupe.index'))
+            return redirect(url_for("groupe.index"))
 
-    return render_template('groupe/create.html')
+    return render_template("groupe/create.html")
+
 
 def get_post(id, check_author=True):
-    post = get_db().execute(
-        'SELECT p.id, titre, description, commentaire, groupe_id, username'
-        ' FROM groupe p JOIN user u ON p.groupe_id = u.id'
-        ' WHERE p.id = ?',
-        (id,)
-    ).fetchone()
+    post = (
+        get_db()
+        .execute(
+            "SELECT p.id, titre, description, groupe_id, username"
+            " FROM groupe p JOIN user u ON p.groupe_id = u.id"
+            " WHERE p.id = ?",
+            (id,),
+        )
+        .fetchone()
+    )
 
     if post is None:
         abort(404, "Groupe id {0} existe pas.".format(id))
 
-    if check_author and post['groupe_id'] != g.user['id']:
+    if check_author and post["groupe_id"] != g.user["id"]:
         abort(403)
 
     return post
 
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+
+@bp.route("/<int:id>/update", methods=("GET", "POST"))
 @login_required
 def update(id):
     post = get_post(id)
 
-    if request.method == 'POST':
-        titre = request.form['titre']
-        description = request.form['description']
+    if request.method == "POST":
+        titre = request.form["titre"]
+        description = request.form["description"]
         # commentaire = request.form['commentaire']
         error = None
 
         if not titre:
-            error = 'Mettre un titre.'
+            error = "Mettre un titre."
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'UPDATE groupe SET titre = ?, description = ?'
-                ' WHERE id = ?',
-                (titre, description, id)
+                "UPDATE groupe SET titre = ?, description = ?" " WHERE id = ?",
+                (titre, description, id),
             )
             db.commit()
-            return redirect(url_for('groupe.index'))
+            return redirect(url_for("groupe.index"))
 
-    return render_template('groupe/update.html', post=post)
+    return render_template("groupe/update.html", post=post)
 
 
-@bp.route('/<int:id>/', methods=('GET', 'POST'))
+@bp.route("/<int:id>/", methods=("GET", "POST"))
 @login_required
 def update2(id):
     post = get_post(id)
 
-    if request.method == 'POST':
-        commentaire = request.form['commentaire']
+    if request.method == "POST":
+        commentaire = request.form["commentaire"]
         error = None
 
         if error is not None:
@@ -102,21 +108,24 @@ def update2(id):
         else:
             db = get_db()
             db.execute(
-                'UPDATE groupe SET commentaire = ?'
-                ' WHERE id = ?',
-                (commentaire, id)
+                "INSERT INTO commentaire (commentaire,groupe_id) VALUES (?,?)",
+                (commentaire, id),
+            )
+            print(
+                "INSERT INTO commentaire (commentaire,groupe_id) VALUES (?,?)",
+                (commentaire, id),
             )
             db.commit()
-            return redirect(url_for('groupe.index'))
+            return redirect(url_for("groupe.index"))
 
-    return render_template('groupe/index.html', post=post)
+    return render_template("groupe/index.html", post=post)
 
 
-@bp.route('/<int:id>/delete', methods=('POST',))
+@bp.route("/<int:id>/delete", methods=("POST",))
 @login_required
 def delete(id):
     get_post(id)
     db = get_db()
-    db.execute('DELETE FROM groupe WHERE id = ?', (id,))
+    db.execute("DELETE FROM groupe WHERE id = ?", (id,))
     db.commit()
-    return redirect(url_for('groupe.index'))
+    return redirect(url_for("groupe.index"))
